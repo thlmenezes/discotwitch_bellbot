71e840b7142945e0ed353d3e34475675977cc18f
Thales Menezes <thlmenezes@pm.me>
2020-12-23 18:10:14 -0300

    feat: add new eslint config

--- src/providers/server.ts
+++ src/providers/server.ts
@@ -4,14 +4,14 @@ import cors from 'cors';
 export class Server {
   public readonly express: express.Application
 
-  public constructor(
+  public constructor (
     routes: Router = Router(),
   ) {
     this.express = express();
     this.setup(routes);
   }
 
-  private setup(routes: Router) {
+  private setup (routes: Router) {
     this.express.use(express.json());
     this.express.use(cors());
     this.express.use('/', routes);
