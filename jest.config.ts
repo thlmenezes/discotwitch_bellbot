27abb6e1dfc8ffab2801d6a4221dc98814ba1bc5
Thales Menezes <thlmenezes@pm.me>
2020-12-23 16:43:41 -0300

    feat: add more jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,19 @@ import { pathsToModuleNameMapper } from 'ts-jest/utils';
 
 export default {
   clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/services/**/*.ts',
+  ],
+  coverageDirectory: '<rootDir>/src/tests/coverage',
+  coverageReporters: [
+    'json',
+    'lcov',
+  ],
   coverageProvider: 'v8',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/src/tests/**/*.spec.ts',
+  ],
 };
