69a9f1ba950b46f4dd6e640677c1d37d52590a3e
Thales Menezes <thlmenezes@pm.me>
2020-12-23 18:46:00 -0300

    feat: first bot code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import { Server as app } from '~/providers/implementations';
+import Discord from 'discord.js';
 
-app.listen(process.env.PORT || 3333);
+const client = new Discord.Client();
+
+client.on('message', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong.');
+  }
+  else if (message.content === '!pong') {
+    message.channel.send('Ping.');
+  }
+  else if (message.content === '!quit' && message.author.username === 'Thales Menezes') {
+    client.destroy();
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
