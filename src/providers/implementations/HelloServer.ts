3966f5ca2ac5bb33cd11970bb96f92cf1abeb6bd
Thales Menezes <thlmenezes@pm.me>
2021-01-03 19:48:07 -0300

    refactor: remove old server test files
    delete old files while mantaining folder structure

--- src/providers/implementations/HelloServer.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { Server } from "~/providers/server";
-
-const router = Router();
-
-router.get("/", (req, res) => res.send("Hello World"));
-
-export default new Server(router).express;
--- src/providers/implementations/index.ts
+++
@@ -1,3 +0,0 @@
-import Server from "./HelloServer";
-
-export { Server };
