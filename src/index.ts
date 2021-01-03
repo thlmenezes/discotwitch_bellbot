f5bb2d4692510a13567781eb37bc508b311fda43
Thales Menezes <thlmenezes@pm.me>
2021-01-03 19:43:23 -0300

    style: apply new quote rule

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import Discord from 'discord.js';
+import Discord from "discord.js";
 
 const client = new Discord.Client();
 
-client.on('message', message => {
-  if (message.content === '!ping') {
-    message.channel.send('Pong.');
+client.on("message", message => {
+  if (message.content === "!ping") {
+    message.channel.send("Pong.");
   }
-  else if (message.content === '!pong') {
-    message.channel.send('Ping.');
+  else if (message.content === "!pong") {
+    message.channel.send("Ping.");
   }
-  else if (message.content === '!quit' && message.author.username === 'Thales Menezes') {
+  else if (message.content === "!quit" && message.author.username === "Thales Menezes") {
     client.destroy();
   }
 });
