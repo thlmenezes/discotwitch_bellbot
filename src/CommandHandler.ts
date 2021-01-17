c3c90a4c4c9ee0ca9bf77e73ce5445599021cd42
Thales Menezes <thlmenezes@pm.me>
2021-01-17 16:14:20 -0300

    feat: add new command - just for fun
    reading discord.js docs and trying new things

--- src/CommandHandler.ts
+++ src/CommandHandler.ts
@@ -3,16 +3,19 @@ import { Message } from "discord.js"
 
 const prefix = "!"
 const commands = new Map([
-  ["ping", "Pong"],
-  ["pong", "Ping"]
+  ["ping", (args: string[]) => "Pong"],
+  ["pong", (args: string[]) => "Ping"],
+  ["teste", (args: string[]) => `args: ${args.join(",")}\nlen(args) ${args.length}`]
 ])
 
 export default function commandHandler (message: Message): void {
-
+  // Filtering
   if (!message.content.startsWith(prefix) || message.author.bot) return
-
-  const args = message.content.slice(prefix.length).trim().split(" ")
+  // Sanitizing
+  const args = message.content.slice(prefix.length).trim().split(/ +/)
   const command = args.shift()?.toLowerCase() || ""
-
-  message.channel.send(commands.get(command) || "Comando Inválido")
+  // Executing
+  const result_command = commands.get(command) || ((arg: string[]) => "Invalid Command")
+  // Echo response
+  message.channel.send(result_command(args))
 }
