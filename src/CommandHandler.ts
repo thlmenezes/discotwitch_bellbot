a231d371499f2639ae44e1a3bb1d900b9650b4da
Thales Menezes <thlmenezes@pm.me>
2021-01-17 17:00:40 -0300

    feat: new command - prune (need permission to delete messages)
    reading the discord.js docs and testing things, will be refactor/deleted at a later date

--- src/CommandHandler.ts
+++ src/CommandHandler.ts
@@ -1,11 +1,24 @@
-import { Message } from "discord.js"
+import { DMChannel, Message } from "discord.js"
 
 
 const prefix = "!"
 const commands = new Map([
-  ["ping", (args: string[]) => "Pong"],
-  ["pong", (args: string[]) => "Ping"],
-  ["teste", (args: string[]) => `args: ${args.join(",")}\nlen(args) ${args.length}`]
+  ["ping", (args: string[], message: Message) => "Pong"],
+  ["pong", (args: string[], message: Message) => "Ping"],
+  ["test", (args: string[], message: Message) => `args: ${args.join(",")}\nlen(args) ${args.length}`],
+  ["prune", (args: string[], message: Message) => {
+    const num = parseInt(args[0])
+    if (isNaN(num)) return "Not a Number"
+
+    if (message.channel instanceof DMChannel) return "Invalid Channel"
+
+    message.channel.bulkDelete(Math.min(num, 10), true).catch(err => {
+      console.error(err)
+      message.channel.send("there was an error trying to prune messages in this channel!")
+    })
+
+    return "done"
+  }]
 ])
 
 export default function commandHandler (message: Message): void {
@@ -17,6 +30,6 @@ export default function commandHandler (message: Message): void {
   // Executing
   const result_command = commands.get(command) || ((arg: string[]) => "Invalid Command")
   // Echo response
-  message.reply(result_command(args))
+  message.reply(result_command(args, message))
   // NOTE: use message.channel.send() to notifications
 }
